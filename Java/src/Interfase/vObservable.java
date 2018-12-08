3780101c904d8087af22436eef6d9f56da5b9201
Federico <federicomariotta@gmail.com>
2018-12-07 21:11:20 -0300

    no message

--- Java/src/Interfase/vObservable.java
+++ Java/src/Interfase/vObservable.java
@@ -45,6 +45,11 @@ public class vObservable extends javax.swing.JFrame implements Observer{
         tblTiposObs = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
 
         tblTiposObs.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -82,14 +87,19 @@ public class vObservable extends javax.swing.JFrame implements Observer{
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(13, Short.MAX_VALUE)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(13, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+        observer obs = new observer();
+        obser.notificar(); 
+    }//GEN-LAST:event_formWindowOpened
 
     /**
      * @param args the command line arguments
